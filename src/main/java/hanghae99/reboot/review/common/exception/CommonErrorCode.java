1a0624b2bf9762bccadc6498a22703ad65b26f66
yuseogi0218 <yuseogi0218@gmail.com>
2024-12-13 09:14:38 +0900

    DELETE - 사용하지 않는 예외 코드 삭제

--- src/main/java/hanghae99/reboot/review/common/exception/CommonErrorCode.java
+++ src/main/java/hanghae99/reboot/review/common/exception/CommonErrorCode.java
@@ -12,17 +12,11 @@ public enum CommonErrorCode implements ErrorCode {
     BAD_REQUEST("COMMON_400_01", HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     MISMATCH_PARAMETER_TYPE("COMMON_400_02", HttpStatus.BAD_REQUEST, "%s 파라미터의 타입이 올바르지 않습니다."),
     REQUIRED_PARAMETER("COMMON_400_03", HttpStatus.BAD_REQUEST, "%s 파라미터는 필수 입력값입니다."),
-    INVALID_PARAMETER("COMMON_400_04", HttpStatus.BAD_REQUEST, "%s"),
 
-    INVALID_REQUEST_BODY_FIELDS("COMMON_400_05", HttpStatus.BAD_REQUEST, "%s"),
-    REQUIRED_REQUEST_BODY("COMMON_400_06", HttpStatus.BAD_REQUEST, "Request Body 가 필요한 요청 입니다."),
+    INVALID_REQUEST_BODY_FIELDS("COMMON_400_04", HttpStatus.BAD_REQUEST, "%s"),
 
     // 405 METHOD_NOT_ALLOWED 허용하지 않은 Http Method
     METHOD_NOT_ALLOWED("COMMON_405_01", HttpStatus.METHOD_NOT_ALLOWED, "해당 요청에는 지원하지 않은 HTTP 메서드 입니다."),
-
-    // 413 PAYLOAD_TOO_LARGE
-    REQUEST_SIZE_EXCEEDED("COMMON_413_01", HttpStatus.PAYLOAD_TOO_LARGE, "요청의 크기가 100MB를 초과합니다."),
-    REQUEST_FILE_SIZE_EXCEEDED("COMMON_413_02", HttpStatus.PAYLOAD_TOO_LARGE, "요청 파일의 크기가 100MB를 초과합니다."),
 
     // 500 INTERNAL SERVER ERROR 서버 에러
     INTERNAL_SERVER_ERROR("COMMON_500_01", HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러 입니다."),
